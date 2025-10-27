c76e5af4b540f1930034f1ee0968471be385c5a9
yj0975 <yjin0975@gmail.com>
2025-10-27 09:41:17 +0900

    Create busan3.js

---
+++ api/busan3.js
@@ -0,0 +1,25 @@
+import { parseStringPromise } from 'xml2js';
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  try {
+    const serviceKey = process.env.SERVICE_KEY;
+    const queryParams = req.query;
+    // const targetUrl = queryParams.targetUrl;
+    // if (!targetUrl) {
+    //   return res.status(400).json({ error: 'targetUrl parameter is missing' });
+    // }
+    // delete queryParams.targetUrl;
+    const searchParams = new URLSearchParams(queryParams);
+    const finalUrl = `https://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteAcctoThrghSttnList?serviceKey=${serviceKey}&${searchParams.toString()}`;
+    const response = await fetch(finalUrl);
+    const xml = await response.text();
+
+    const json = await parseStringPromise(xml, { explicitArray: false });
+
+    res.status(200).json(json);
+
+  } catch (e) {
+    res.status(500).json({ error: e.toString() });
+  }
+}
